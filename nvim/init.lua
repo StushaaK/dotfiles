6085778305abce51ce2d5b13d50a310461f88b72
Daniel Stus <daniel.stus@siemens.com>
2024-02-27 15:13:30 +0100

    feat: add neovim config files

---
+++ nvim/init.lua
@@ -0,0 +1,22 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.filetype.add {
+    extension = {
+        astro = "astro"
+    },
+}
+
+require("set")
+require("map")
+require("lazy").setup("plugins")
